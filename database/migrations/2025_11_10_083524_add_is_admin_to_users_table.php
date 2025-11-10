7d8576766d12b79010eafae8482accc7c6e7b002
cornel-s-k <cornel.lydia@gmail.com>
2025-11-10 15:35:55 +0700

    udfsvguysd saya lelah

---
+++ database/migrations/2025_11_10_083524_add_is_admin_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Tambahkan kolom 'is_admin' dengan nilai default false (0)
+            $table->boolean('is_admin')->default(false)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Hapus kolom 'is_admin'
+            $table->dropColumn('is_admin');
+        });
+    }
+};
